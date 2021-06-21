b4b6f9dc5b7b26dc785a005ccc20c06d37cd2a56
mmoha <akanush16@gmail.com>
2021-06-21 22:06:53 +0530

    1st sample code push to git

---
+++ basicConcepts/basicConcepts/printFunction.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package basicConcepts;
+
+/**
+ * @author mmoha
+ *
+ */
+
+/*this is function to display names
+*/
+public class printFunction {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.print("My name is Sara");
+		System.out.print(" My name is hamza");
+		System.out.print(" My name is airah");
+		System.out.print(" My name is aizah");
+
+	}
+
+}
